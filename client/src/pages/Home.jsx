a58b5aedfd15888394cc2b731f2d464050ab704e
Juan Aguilera <64390726+ElPinguiino@users.noreply.github.com>
2023-04-14 22:35:58 -0600

    fixes for deployment

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -21,21 +21,16 @@ const Home = () => {
       {snap.intro && (
         <motion.section className="home" {...slideAnimation('left')}>
           <motion.header {...slideAnimation("down")}>
-            <img 
-              src='./threejs.png'
-              alt="logo"
-              className="w-8 h-8 object-contain"
-            />
           </motion.header>
           <motion.div className="home-content" {...headContainerAnimation}>
             <motion.div {...headTextAnimation}>
               <h1 className="head-text">
-                LET'S <br className="xl:block hidden" /> DO IT.
+                T-SHIRT <br className="xl:block hidden" /> CUSTOMIZER.
               </h1>
             </motion.div>
             <motion.div className="flex flex-col gap-5" {...headContentAnimation}>
               <p className="max-w-md font-normal text-gray-600 text-base">
-                Create your unique and exclusive shirt with our brand new 3D customization tool. <strong>Unleash your imagination</strong>{""}
+                Create your unique and exclusive shirt with this brand new 3D customization tool. <strong>Unleash your imagination </strong>{""} 
                 and define your own style.
               </p>
               <CustomButton 
